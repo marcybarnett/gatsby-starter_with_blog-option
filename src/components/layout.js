3a69ec5ff0ed01d0dbec4f1b4cac54c27541c54b
marcybarnett <barnettconcepts@gmail.com>
2020-07-03 21:08:30 -0600

    started stripping all the fluff out

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,15 +25,10 @@ const Layout = ({ children }) => {
   `)
 
   return (
+    /* eslint-disable */
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
